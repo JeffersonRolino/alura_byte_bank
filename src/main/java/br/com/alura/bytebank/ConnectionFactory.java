bb025ca3433bcdde51072a9afff3423cb387d089
jeffersonrolino <jefferson.rolino@gmail.com>
2024-05-13 06:39:40 -0300

    Curso 01 - Capítulo 02 - Aula 02: Isolando conexões com factory

--- src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -4,23 +4,18 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConexaoDB {
-    public static void main(String[] args) {
+public class ConnectionFactory {
+
+    public Connection recuperarConexao(){
         String userDB = System.getenv("MYSQL_USERNAME");
         String passwordDB = System.getenv("MYSQL_PASSWORD");
 
         try {
-            Connection connection = DriverManager.getConnection(
+            return DriverManager.getConnection(
                     String.format( "jdbc:mysql://localhost:3306/byte_bank?user=%s&password=%s", userDB, passwordDB)
             );
-
-            System.out.println("Connection with the Database OPEN");
-
-            connection.close();
-
         } catch (SQLException exception){
-            System.out.println("Failure to connect with the Database: " + exception);
+            throw new RuntimeException(exception);
         }
-
     }
 }
