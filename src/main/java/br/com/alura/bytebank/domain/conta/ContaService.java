3724a20b00d350b5faeca3da1f53100cc7eee625
jeffersonrolino <jefferson.rolino@gmail.com>
2024-05-13 07:01:51 -0300

    Curso 01 - Capítulo 02 - Aula 04: Preparando para salvar conta

--- src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
+++ src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
@@ -1,13 +1,23 @@
 package br.com.alura.bytebank.domain.conta;
 
+import br.com.alura.bytebank.ConnectionFactory;
 import br.com.alura.bytebank.domain.RegraDeNegocioException;
 import br.com.alura.bytebank.domain.cliente.Cliente;
 
 import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
 
 public class ContaService {
+
+    private ConnectionFactory connectionFactory;
+
+    public ContaService() {
+        this.connectionFactory = new ConnectionFactory();
+    }
 
     private Set<Conta> contas = new HashSet<>();
 
@@ -27,7 +37,24 @@ public class ContaService {
             throw new RegraDeNegocioException("Já existe outra conta aberta com o mesmo número!");
         }
 
-        contas.add(conta);
+        String sql = "INSERT INTO conta (numero, saldo, cliente_nome, cliente_cpf, cliente_email)" +
+                "VALUES (?, ?, ?, ?, ?)";
+
+        Connection connection = connectionFactory.recuperarConexao();
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, conta.getNumero());
+            preparedStatement.setBigDecimal(2, BigDecimal.ZERO);
+            preparedStatement.setString(3, cliente.getNome());
+            preparedStatement.setString(4, cliente.getCpf());
+            preparedStatement.setString(5, cliente.getEmail());
+
+            preparedStatement.execute();
+
+        } catch (SQLException exception){
+            throw new RuntimeException(exception);
+        }
     }
 
     public void realizarSaque(Integer numeroDaConta, BigDecimal valor) {
