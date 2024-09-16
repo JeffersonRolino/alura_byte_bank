d38dd9b1dba4a0c08c46af6c2428ef61f70e9b5c
jeffersonrolino <jefferson.rolino@gmail.com>
2024-09-16 03:56:40 -0300

    Curso 01 - Capítulo 03 - Aula 06: Escalabilidade de conexões

--- src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -1,21 +1,31 @@
 package br.com.alura.bytebank;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-
     public Connection recuperarConexao(){
-        String userDB = System.getenv("MYSQL_USERNAME");
-        String passwordDB = System.getenv("MYSQL_PASSWORD");
-
         try {
-            return DriverManager.getConnection(
-                    String.format( "jdbc:mysql://localhost:3306/byte_bank?user=%s&password=%s", userDB, passwordDB)
-            );
+            return createDataSource().getConnection();
         } catch (SQLException exception){
             throw new RuntimeException(exception);
         }
+    }
+
+    private HikariDataSource createDataSource(){
+        String userDB = System.getenv("MYSQL_USERNAME");
+        String passwordDB = System.getenv("MYSQL_PASSWORD");
+
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/byte_bank");
+        config.setUsername(userDB);
+        config.setPassword(passwordDB);
+        config.setMaximumPoolSize(10);
+
+        return new HikariDataSource(config);
     }
 }
